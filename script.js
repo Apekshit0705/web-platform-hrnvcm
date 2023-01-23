93f317a2873fedb44647bf67cf319ffb48da7699
Apekshit0705 <116092846+Apekshit0705@users.noreply.github.com>
2023-01-23 15:58:36 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log('Hello!');
+const game = document.getElementById("game");
+const startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  startButton.style.display = "none";
+  
+  //create game objects
+  const player = createGameObject("player", 50, 50);
+  game.appendChild(player);
+  
+  const enemy = createGameObject("enemy", 100, 100);
+  game.appendChild(enemy);
+  
+  //add event listeners for player movement
+  document.addEventListener("keydown", movePlayer);
+  
+  function movePlayer(event) {
+    switch (event.keyCode) {
+      case 37: //left arrow
+        player.style.left = player.offsetLeft - 10 + "px";
+        break;
+      case 38: //up arrow
+        player.style.top = player.offsetTop - 10 + "px";
+        break;
+      case 39: //right arrow
+        player.style.left = player.offsetLeft + 10 + "px";
+        break;
+      case 40: //down arrow
+        player.style.top = player.offsetTop + 10 + "px";
+        break;
+    }
+  }
+  
+  function createGameObject(className, x, y) {
+    const gameObject = document.createElement("div");
+    gameObject.classList.add(className);
+    gameObject.style.left = x + "px";
+    gameObject.style.top = y + "px";
+    return gameObject;
+  }
+}
